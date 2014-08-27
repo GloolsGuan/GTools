869a9b7aa61dbc8af8fe0956ec9e3eaf15796f2d
GloolsGuan <GloolsGuan@glools.com>
2014-08-27 16:17:39 +0800

    Delete activities view components.

--- webroot/sites/all/views/custom/activities/misc/slide/slide.view.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    var options = {
-        'autoScroll' : 'no'
-    };
-    
-    jQuery('#region-sidebar_top #view_custom_activities_slide').tabScroller(options);
-});
